459e488e171938c4d9e34e1cfc1873515aa7d570
Nieltom Augusto <120043427+nieltom@users.noreply.github.com>
2023-05-11 17:29:35 -0300

    Initial commit

---
+++ src/JS/index.js
@@ -0,0 +1,31 @@
+
+const personagens = document.querySelectorAll(".personagem");
+
+personagens.forEach((personagem) => {
+  personagem.addEventListener("mouseenter", () => {
+
+    const idSelecionado = personagem.attributes.id.value;
+
+    if(idSelecionado === 'ultron') return;
+
+    const personagemSelecionado = document.querySelector(".selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+
+    personagem.classList.add("selecionado");
+
+    const imagemJogador1 = document.getElementById('personagem-jogador-1');
+    imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
+
+    const nomeJogador1 = document.getElementById('nome-jogador-1');
+    const nomeSelecionado = personagem.getAttribute('data-name');
+    
+    nomeJogador1.innerHTML = nomeSelecionado;
+  });
+});
+
+document.getElementById('inosuke').addEventListener('click', function() {
+  document.getElementById('inosukesom').style.display = 'none';
+  document.getElementById('inosukesom').play();
+});
+
+
